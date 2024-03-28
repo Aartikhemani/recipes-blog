5df96783d68285ba03681a1432a28d108c579c68
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-03-28 11:28:37 +0100

    register models on admin

--- backend/nutrition_facts/admin.py
+++ backend/nutrition_facts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import NutritionFacts
+
+
+@admin.register(NutritionFacts)
+class NutritionFactsAdmin(admin.ModelAdmin):
+    pass
