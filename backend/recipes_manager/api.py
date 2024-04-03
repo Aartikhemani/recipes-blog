7fcff442178f6f939dc5b3c7fdd09046d2d61ecb
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-04-03 13:55:07 +0200

    modify ingredients API

--- backend/recipes_manager/api.py
+++ backend/recipes_manager/api.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
 from recipes_manager import views
+from nutrition_facts.views import IngredientViewSet
 
 app_name = 'recipes-manager'
 
 router = DefaultRouter()
 router.register(r'categories', views.CategoryViewSet, basename='categories')
 router.register(r'recipes', views.RecipeViewSet, basename='recipes')
-router.register(r'ingredients', views.IngredientViewSet, basename='ingredients')
+router.register(r'ingredients', IngredientViewSet, basename='ingredients')
 router.register(r'ingredients-recipes', views.IngredientRecipeViewSet, basename='ingredients-recipes')
--- backend/recipes_manager/serializers.py
+++ backend/recipes_manager/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Recipe, Category, IngredientRecipe, Ingredient
+from .models import Recipe, Category, IngredientRecipe
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -11,12 +11,6 @@ class RecipeSerializer(serializers.ModelSerializer):
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
-
-
-class IngredientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ingredient
         fields = "__all__"
 
 
