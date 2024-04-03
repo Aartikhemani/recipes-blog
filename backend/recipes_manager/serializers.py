4ca23c92e073c38f681ec09d201be733b392fe88
ElaKaptsilava <lizavetakaptsilava@gmail.com>
2024-04-03 11:16:43 +0200

    recipes_manager serializers

---
+++ backend/recipes_manager/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Recipe, Category, IngredientRecipe, Ingredient
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
+
+
+class IngredientRecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IngredientRecipe
+        fields = '__all__'
